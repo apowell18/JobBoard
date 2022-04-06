911cbd57d192514b44f25831225a8b4ff5e87dfe
alexis <alexis_ceres@hotmail.com>
2022-04-06 11:35:41 -0400

    Updates

--- main.py
+++ main.py
@@ -1,30 +1,45 @@
-from flask import Flask
+from flask import Flask, render_template
 
 app = Flask(__name__)
 
+#create dictionary
+buisnessPosts = [
+    {
+        'company_name': "Walmart",
+        'job_title': "Data Analyst",
+        'job_location': "California",
+        'content': "Here is the requirements",
+        'job_type': "Full-Time",
+        'job_description': "Conduct advanced statistical analysis to provide actionable insights, identify trends, and measure performance.",
+        'job_qualifications': "Must have Bachelors in Statistics, Computer Science, or anything realted",
+        'salary': "$22 per hour",
+        'contact': "https://www.youtube.com",
+        'date_posted':'May 05, 2021'
+    }
+]
 @app.route("/")
 def home():
-    return "<h1>Home Page</h1>"
+    return render_template("home.html", bPosts = buisnessPosts)
 
 @app.route("/about")
 def about():
-    return "<h1>About Page</h1>"
+    return render_template("about.html", title='About')
 
 @app.route("/login")
 def login():
-    return "<h1>login Page</h1>"
+    return render_template("login.html", title='Login')
 
 @app.route("/signup")
 def signUp():
-    return "<h1>signup Page</h1>"
+    return render_template("signup.html", title='Registeration')
 
-@app.route("/createBoard")
+@app.route("/createboard")
 def createBoard():
-    return "<h1>Create Job Board Page</h1>"
+    return render_template("createboard.html", title='Create Job Board')
 
-@app.route("/")
-def about():
-    return "<h1>About Page</h1>"
+@app.route("/profile")
+def profile():
+    return render_template("profile.html", title='Profile')
 
 if __name__ == '__main__':
     app.run(debug=True)
